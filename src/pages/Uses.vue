e6bd5e550a82de2f9f6c8af02765887f24bec43f
Jason Cockerham <6616625+jcock@users.noreply.github.com>
2020-03-10 13:30:55 -0400

    Color adjustments; Threw some stuff on Uses page

--- src/pages/Uses.vue
+++ src/pages/Uses.vue
@@ -1,7 +1,14 @@
 <template>
 	<layout>
-		<h1>Uses</h1>
-   	<h2>Hardware/Software that I use.</h2>
+		<section class="intro">
+			<figure class="desk">
+				<g-image src="~/assets/img/keeb.webp" alt="My battlestation" class="photo" />
+			</figure>
+			<article class="content">
+				<h1>Uses</h1>
+   			<p>Hardware/software that I use most. I change up things fairly often, so this page will serve as a living document.</p>
+			</article>
+		</section>
 	</layout>
 </template>
 
@@ -13,3 +20,53 @@ export default {
 	}
 }
 </script>
+
+<style scoped>
+.intro {
+	display: grid;
+	grid-gap: var(--step-0);
+	grid-template: auto / auto;
+	justify-items: center;
+
+	@media screen and (min-width: 62rem) {
+		align-items: stretch;
+		grid-template-columns: 6fr 1fr 5fr;
+		justify-items: start;
+	}
+}
+
+.content {
+	align-self: center;
+	grid-row: 3 / 4;
+	max-width: 38rem;
+	z-index: 2;
+
+	@media screen and (min-width: 62rem) {
+		grid-area: 1 / 2 / 2 / 5;
+		max-width: 45rem;
+		padding-left: var(--step-0);
+	}
+}
+
+.title {
+	margin-top: 0;
+}
+
+.desk {
+	grid-row: 1 / 2;
+	margin: 0;
+	width: 100%;
+
+	@media screen and (min-width: 62rem) {
+		grid-area: 1 / 1 / 2 / 3;
+		opacity: 0.33;
+	}
+}
+
+.photo {
+	object-fit: cover;
+	object-position: 50%;
+	height: 100%;
+	width: 100%;
+}
+</style>
