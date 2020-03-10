ec2efab8bd99e2addc511aea86dfc933bbaa3519
Jason Cockerham <6616625+jcock@users.noreply.github.com>
2020-03-10 10:47:21 -0400

    standby: another test

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,5 +1,5 @@
 const cssImport = require('postcss-import');
-// const presetEnv = require('postcss-preset-env');
+const presetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
 const purgecss = require('@fullhuman/postcss-purgecss');
 
@@ -10,17 +10,84 @@ const isProduction = process.env.NODE_ENV === 'production';
 // PostCSS config
 const postcssPlugins = [
 	cssImport(),
-	// presetEnv({
-	// 	stage: 0,
-	// 	features: {
-	// 		'custom-properties': false
-	// 	}
-	// }),
+	presetEnv({
+		stage: 0,
+		features: {
+			// 'custom-properties': false
+			'all-property': false,
+			'any-link-pseudo-class': false,
+			'blank-pseudo-class': false,
+			'break-properties': false,
+			'case-insensitive-attributes': false,
+			'color-functional-notation': false,
+			'color-mod-function': false,
+			'custom-media-queries': false,
+			'custom-properties': false,
+			'custom-selectors': false,
+			'dir-pseudo-class': false,
+			'double-position-gradients': false,
+			'environment-variables': false,
+			'focus-visible-pseudo-class': false,
+			'focus-within-pseudo-class': false,
+			'font-variant-property': false,
+			'gap-properties': false,
+			'gray-function': false,
+			'has-pseudo-class': false,
+			'hexadecimal-alpha-notation': false,
+			'image-set-function': false,
+			'lab-function': false,
+			'logical-properties-and-values': false,
+			'matches-pseudo-class': false,
+			'media-query-ranges': false,
+			'nesting-rules': false,
+			'not-pseudo-class': false,
+			'overflow-property': false,
+			'overflow-wrap-property': false,
+			'place-properties': false,
+			'prefers-color-scheme-query': false,
+			'rebeccapurple-color': false,
+			'system-ui-font-family': false
+		}
+	}),
 	cssnano({
 		preset: [
 			'default',
 			{
-				calc: false
+				// calc: false
+				autoprefixer: false,
+				cssDeclarationSorter: false,
+				rawCache: false,
+				calc: false,
+				colormin: false,
+				convertValues: false,
+				discardComments: false,
+				discardDuplicates: false,
+				discardEmpty: false,
+				discardOverridden: false,
+				discardUnused: false,
+				mergeIdents: false,
+				mergeLonghand: false,
+				mergeRules: false,
+				minifyFontValues: false,
+				minifyGradients: false,
+				minifyParams: false,
+				minifySelectors: false,
+				normalizeCharset: false,
+				normalizeDisplayValues: false,
+				normalizePositions: false,
+				normalizeRepeatStyle: false,
+				normalizeString: false,
+				normalizeTimingFunctions: false,
+				normalizeUnicode: false,
+				normalizeUrl: false,
+				normalizeWhitespace: false,
+				orderedValues: false,
+				reduceIdents: false,
+				reduceInitial: false,
+				reduceTransforms: false,
+				svgo: false,
+				uniqueSelectors: false,
+				zindex: false
 			}
 		]
 	})
